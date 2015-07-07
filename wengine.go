9cde177c8c391172d35e1507c8c3b0cef0e02225
Marat <tourervs@gmail.com>
2015-07-07 11:35:11 +0300

    fourth commit

--- wengine.go
+++ wengine.go
@@ -2,15 +2,53 @@
 package main
 
 import (
-	"io"
-	"net/http"
+    "fmt"
+    "net/http"
+    "io/ioutil"
+//    "errors"
+//    "fmt"
+    "os"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
-}
+
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
+
+    port , _, _ := get_args()
+
+    port = ":"+port
+
+    http.HandleFunc("/", homeHandler)
+    panic(http.ListenAndServe(port, nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    _,filepath,_:=get_args()
+    text , _ := readFile(filepath)
+    fmt.Fprintf(w, text)
+
+}
+
+func readFile(filename string) (text string ,err error ) {
+    rawBytes, err := ioutil.ReadFile(filename)
+    text = string(rawBytes)
+    return text, nil
+}
+
+func get_args()(port string,filepath string , err error) {
+
+
+    if len(os.Args) == 3 {
+
+        port = os.Args[1]
+        filepath = os.Args[2]
+        if inFile, err := os.Open(filepath); err != nil{
+            defer inFile.Close()
+            return "","",err
+        }
+        }
+        return port,filepath,nil
+
+
+
 }
