ae9aad367bb88724755c861e5e1b406741bc6b4f
Marat <tourervs@gmail.com>
2015-07-14 19:35:33 +0300

    14072015-1935

--- wengine_parts/repositories.go
+++ wengine_parts/repositories.go
@@ -17,29 +17,43 @@ type Package struct {
     Markers  []string
 }
 
+
+
 type DebPackage struct {
 
     Package string
     Source string
     Priority string
     Section string
-    Installed-Size int
+    InstalledSize int
     Maintainer string
     Architecture string
     Version string
-Depends: ceilometer-common (= 2015.1.0-0ubuntu1~cloud0), init-system-helpers (>= 1.13~), sysv-rc (>= 2.88dsf-24) | file-rc (>= 0.8.16)
-, python:any
-Supported: 18m
-Filename: pool/main/c/ceilometer/ceilometer-agent-central_2015.1.0-0ubuntu1~cloud0_all.deb
-Size: 4862
-MD5sum: ada1d7f4bddcce586543de615f0d9eac
-SHA1: 3b5b5faa0cdc2b2626938b941f9604cd33b53006
-SHA256: df879c9da8ba89c593698f84c6a407e3d6a1cc091558eb5ff80e48020454c488
-Description: ceilometer central agent
-Description-md5: 69931ca0e99876c83677bf349c97e7bb
+    Depends []DependencyDeb
+    Supported string
+    Filename string 
+    Size int
+    MD5sum string
+    SHA1 string 
+    SHA256 string 
+    Description string 
+    Descriptionmd5 string 
+}
+
+type DependencyDeb struct {
+
+    Package string
+    Version string
+    Substitutes []string  
+
+}
+
+
+
+type RpmPackage struct {
+
 
 
 
 }
-
 
