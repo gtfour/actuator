dff9bd559eb7d22efcee963774bfefc60b1810b1
root <root@zombie.(none)>
2015-08-05 00:20:31 +0300

    04082015-0120

--- wengine_parts/src/wengine_parts/dbsync.go
+++ wengine_parts/src/wengine_parts/dbsync.go
@@ -2,8 +2,8 @@ package main
 import (
         "fmt"
         "log"
-        //"gopkg.in/mgo.v2"
-        //"gopkg.in/mgo.v2/bson"
+        "gopkg.in/mgo.v2"
+        "gopkg.in/mgo.v2/bson"
         "wengine_parts/repository"
 )
 func main() {
