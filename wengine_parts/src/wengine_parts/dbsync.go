be042507cc0853d18d622aa81ae3745bfd01a7eb
root <root@zombie.(none)>
2015-08-05 22:43:07 +0300

    040815-2243

--- wengine_parts/src/wengine_parts/dbsync.go
+++ wengine_parts/src/wengine_parts/dbsync.go
@@ -1,16 +1,23 @@
 package main
 import (
+
         "fmt"
-        "log"
-        "gopkg.in/mgo.v2"
-        "gopkg.in/mgo.v2/bson"
+
+        //"log"
+
+        //"gopkg.in/mgo.v2"
+
+        //"gopkg.in/mgo.v2/bson"
+
         "wengine_parts/repository"
 )
 func main() {
 
-fmt.Println("====")
+    fmt.Println("====")
 
+    my_repo_relation:= repository.RpmRelation {Name: "/bin/bash"}
 
+    fmt.Println(my_repo_relation)
 
 }
 
