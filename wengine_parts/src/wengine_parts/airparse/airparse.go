22d5ffbf83d2c6829da08fce56c0ceae27569dc7
Marat <tourervs@gmail.com>
2015-08-07 15:56:29 +0300

    07082015-1556

--- wengine_parts/src/wengine_parts/airparse/airparse.go
+++ wengine_parts/src/wengine_parts/airparse/airparse.go
@@ -1,1 +1,39 @@
 package airparse
+
+import ("net/http";"io";"errors")
+
+var err = errors.New("airparse side error")
+
+type RepoFile struct {
+
+    Id string
+    FileName string
+    Url string
+    DataGZ http.Response
+    DataXML http.Response
+    Type string // deb or rpm
+
+}
+
+func (repofile *RepoFile) download(){
+
+    repofile.DataGZ=*http.Get(repofile.Url)
+
+}
+
+func (repofile *RepoFile) extract(){
+
+
+}
+
+func (repofile *RepoFile) parse(){
+
+}
+
+func (repofile *RepoFile) handle(){
+
+
+}
+
+
+
