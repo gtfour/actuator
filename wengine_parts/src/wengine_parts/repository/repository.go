992d0ec840071c7d0b6ecce6fd350bdb288138bc
root <root@zombie.(none)>
2015-08-15 21:29:02 +0300

    14082015-2228

--- wengine_parts/src/wengine_parts/repository/repository.go
+++ wengine_parts/src/wengine_parts/repository/repository.go
@@ -1,8 +1,39 @@
 package repository
 
+type Repository struct {
+
+    Id string
+    Url string //"http://linux.dell.com/repo/community/content/el5-i386/"
+    Type string // deb or rpm
+    Mtime string
+
+}
+
 type RpmRepository struct {
-    Url string "http://linux.dell.com/repo/community/content/el5-i386/"
-    //Metadata 
-    //Packages 
+
+    Repository
+    PrimaryXml string "repodata/primary.xml.gz"
+
+}
+
+type DebRepository struct {
+
+    Repository
+    Packages string "repodata/Packages.gz"
+
+}
+
+
+
+
+type Info struct {
+
+
+}
+
+type RepoMarker struct {
+
+
+
 }
 
