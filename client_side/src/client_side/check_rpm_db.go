419ffd4a86a618b641ee4aa20f0cff97e870fdf2
Marat <tourervs@gmail.com>
2015-08-18 19:53:36 +0300

    18082015-1953

---
+++ client_side/src/client_side/check_rpm_db.go
@@ -0,0 +1,29 @@
+package main
+
+import "client_side/protodb"
+import "fmt"
+
+var packages_db_file = "/var/lib/rpm/Packages"
+var environment_dir = "/var/lib/rpm"
+
+func main(){
+
+environment_config:=&protodb.EnvironmentConfig{Create:false}
+
+environment,_:=protodb.OpenEnvironment(environment_dir,environment_config)
+
+transaction:=protodb.NoTransaction
+
+//database_config:=DatabaseConfig{Create:false}
+
+database,_ :=protodb.OpenDatabase(environment,transaction,packages_db_file,&protodb.DatabaseConfig{Create:false})
+
+defer database.Close()
+
+database_type,_:=database.Type()
+fmt.Println(database_type)
+
+
+}
+
+
---
+++ profile.sh
@@ -0,0 +1,1 @@
+export GOPATH=/actuator/wengine_parts:/actuator/client_side:/usr/share/go
