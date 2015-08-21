a76c072696d17911f62e9a9a9a7a81698adce479
Marat <tourervs@gmail.com>
2015-08-21 15:51:28 +0300

    21082015-1551

--- client_side/src/client_side/deb_check/check_installed.go
+++ client_side/src/client_side/deb_check/check_installed.go
@@ -1,3 +1,37 @@
 package deb_check
 
+type DebPackageStatusEntry struct {
+
+    Name string
+    Version string
+    Architecture string
+
+}
+
+type DpkgStatusFile struct {
+
+    FilePath string
+    Sum string
+
+}
+
+func OpenStatusFile() {
+
+}
+
+func Watch() {
+
+
+}
+
+func CheckSum() {
+
+
+}
+
+func GenSumModifiedEvent() {
+
+
+}
+
 // main info /var/lib/dpkg/status
