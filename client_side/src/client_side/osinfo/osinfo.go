8900280285a2c8f6ee7fe5f7bbdfefddb167f57f
root <root@zombie.(none)>
2015-08-27 01:22:33 +0300

    26082015-0222

--- client_side/src/client_side/osinfo/osinfo.go
+++ client_side/src/client_side/osinfo/osinfo.go
@@ -3,37 +3,53 @@ package main
 import "fmt"
 import "os"
 import "bufio"
-
+import "io"
+// I am glad to introduce you "f-shit power" and "How does this fking code work" technologies 
 
 
 
 type OS struct {
 
-    HostName string
+    Hostname string
     OsName string
     OsVersion string
     OsRelease string
     Files []string
-
-}
-
-func (os *OS) CheckEtcFiles() (err error) {
-
-    fmt.Println(os.Files)
-
-    return nil
+    VirtualProvider []string // по задумке сюда будут складываться строки начиная со второй, которые были найдены
+                             // проходом значений хеша по ключу директ 
+                             // по идее директ должен включать файлы имеющие всего одну строку, не содержащую ключей а только одно значение
+                             // прошу прощения за этот бред )
 
 }
 
 
-func (os *OS) Hostname() (err error) {
+
+func main() {
+
+   operating_system:=&OS{}
+
+   operating_system.GetHostname()
+   fmt.Println(operating_system.Hostname)
+   fmt.Println(operating_system.VirtualProvider)
+
+   fmt.Println("----")
+
+}
 
 
+
+func (os *OS) GetHostname() (err error) {
+
+                                          // по ключу директ находятся однострочные файлы
+                                          // по ключу комплекс - файлы которые надо парсить на предмет наличия внутри complex_key
     var providers =  map[string][]string  {"direct":{"/proc/sys/kernel/hostname","/etc/hostname","/etc/HOSTNAME"}, "complex":{"/etc/sysconfig/network"}}
     var complex_keys = []string {"HOSTNAME"}
 
-    return nil
+    //fmt.Printf("\n:debug:\n%s\n%s",providers,complex_keys)
 
+    os.Hostname,_,os.VirtualProvider=GetParamValue(providers,complex_keys)
+
+    return nil
 
 }
 
@@ -44,7 +60,9 @@ func (os *OS) Name() (err error) {
 
     var complex_keys = []string {"NAME"}
 
+    fmt.Printf("\n:debug:\n%s\n%s",providers,complex_keys)
     return nil
+
 
 
 }
@@ -55,8 +73,8 @@ func (os *OS) Version() (err error) {
 
     var complex_keys = []string {"VERSION_ID","VERSION","release"}
 
+    fmt.Printf("\n:debug:\n%s\n%s",providers,complex_keys)
     return nil
-
 
 }
 
@@ -68,6 +86,7 @@ func (os *OS) Release() (err error) {
 
     var complex_keys = []string {"release","DISTRIB_RELEASE"}
 
+    fmt.Printf("\n:debug:\n%s\n%s",providers,complex_keys)
     return nil
 
 
@@ -75,23 +94,14 @@ func (os *OS) Release() (err error) {
 
 
 
+func GetParamValue(providers map[string][]string , complex_keys []string) (value string,err error,vp []string) {
 
-func main() {
+    //    var delimiters = []string {"="," ",": "}
 
-   operating_system:=&OS{OsName:"ubuntu",Files:[]string{"/etc/release"}}
-
-   fmt.Println("----")
-
-   fmt.Println(operating_system)
-
-   fmt.Println("----")
-
-}
-
-func ReadFile(providers map[string][]string , complex_keys []string) (value string,err error) {
-
-    var delimiters = []string {"="," ",": "}
     value = "Unknown"
+
+    var possible_value_candidates []string
+
     for provider_type := range providers {
 
         if provider_type == "direct" {
@@ -99,9 +109,20 @@ func ReadFile(providers map[string][]string , complex_keys []string) (value stri
             for name := range providers["direct"] {
 
                 filename := providers["direct"][name]
-                file, err := os.Open(filename)
+
+                lines,err:=ReadFileLines(filename)
+
+                
+
                 if err==nil {
-                     buffered_reader:=bufio.NewReader(file)
+
+                     fmt.Println(len(lines))
+
+                     lines_len :=len(lines)
+
+                     if lines_len > 0 { possible_value_candidates=append(possible_value_candidates,lines[0]) }
+
+                     if lines_len > 1 { for i:= range lines { vp=append(vp,lines[i]) } }
 
                 }
 
@@ -113,7 +134,46 @@ func ReadFile(providers map[string][]string , complex_keys []string) (value stri
 
 
     }
-    return "",nil
+    return "",nil,vp
 
 
 }
+
+
+func ReadFileLines (filename string) (lines []string,err error){
+
+    file, err := os.Open(filename)
+
+    if err!=nil {
+
+        return lines, err
+
+    }
+
+    buffered_reader:=bufio.NewReader(file)
+
+    eof := false
+
+    for lino := 1; !eof; lino++ {
+
+        line, err := buffered_reader.ReadString('\n')
+
+        if err == io.EOF {
+
+            err = nil
+
+            eof = true
+
+        } else if err != nil {
+
+            return lines, err
+
+        }
+
+        lines=append(lines,line)
+
+    }
+    return lines,err
+
+}
+
