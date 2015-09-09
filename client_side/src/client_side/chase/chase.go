a7b148410e1a2fa04058b59bb1e3cc494d42936b
Marat <tourervs@gmail.com>
2015-09-09 09:49:50 +0300

    09092015-0949

---
+++ client_side/src/client_side/chase/chase.go
@@ -0,0 +1,22 @@
+package chase
+
+
+
+type Target struct {
+    Path string
+    OldSum string
+    Sum string
+    Modified bool
+}
+
+func(tgt *Target) Chasing (){
+
+
+}
+
+func (tgt *Target) Reporting (){
+
+    //UpdateConfFile()
+    //SendPostRequest()
+
+}
