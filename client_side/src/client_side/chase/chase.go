15a698b31c58c9054cc89103ef78efa0173ef4db
Marat <tourervs@gmail.com>
2015-09-09 16:49:17 +0300

    09092015-1648

--- client_side/src/client_side/chase/chase.go
+++ client_side/src/client_side/chase/chase.go
@@ -3,13 +3,28 @@ package chase
 
 
 type Target struct {
+
     Path string
     OldSum string
     Sum string
     Modified bool
+
 }
 
-func(tgt *Target) Chasing (){
+func Start (targets []string)(err error){
+
+    return nil
+
+}
+
+func Stop()(err error) {
+
+
+    return nil
+}
+
+
+func (tgt *Target) Chasing (){
 
 
 }
