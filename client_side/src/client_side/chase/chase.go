32fafcb7e467f096ec184ab6fd11dc03ff77e38f
Marat <tourervs@gmail.com>
2015-09-10 20:01:16 +0300

    10092015-2000

--- client_side/src/client_side/chase/chase.go
+++ client_side/src/client_side/chase/chase.go
@@ -1,5 +1,7 @@
 package chase
 
+import "client_side/actuator"
+
 
 
 type Target struct {
@@ -14,6 +16,36 @@ type Target struct {
 }
 
 func Start (targets []string)(err error){
+
+    for id :=range targets {
+
+    file_struct,err:= actuator.Get_md5_file(targets[id])
+    if err!=nil {
+
+        dir_struct,err:=actuator.Get_md5_dir(targets[id])
+        if err==nil {
+
+            for file_id :=range dir_struct.Files{
+
+                target:=&Target{}
+                target.Path=dir_struct.Files[file_id].Path
+                target.OldSum=string(dir_struct.Files[file_id].Sum)
+                go target.Chasing()
+
+
+            }
+
+        }
+    }else {
+
+      target:=&Target{}
+      target.Path=targets[id]
+      target.OldSum=string(file_struct.Sum)
+       go target.Chasing()
+
+
+    }
+    }
 
     return nil
 
