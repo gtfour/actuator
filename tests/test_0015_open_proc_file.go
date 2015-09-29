0022c3a66c711df6d6b43cf121363873625b5690
Marat <tourervs@gmail.com>
2015-09-29 13:07:06 +0300

    29092015-1306

--- tests/test_0015_open_proc_file.go
+++ tests/test_0015_open_proc_file.go
@@ -9,51 +9,56 @@ import "time"
 
 func main() {
 
-    filename:="/proc/1/task/1/cwd/proc/kmsg"
-    //filename:="/proc/1/task/1/cwd/proc/partitions"
-    file, err := os.Open(filename)
+    //filename:="/proc/1/task/1/cwd/proc/kmsg"      //empty not readable file
+    filename:="/proc/1/task/1/cwd/proc/partitions"  //empty readable file
 
+    //
+    file, err := os.Open(filename)
       if err!=nil {
         return
     }
+    readable:=make(chan bool,1)
+    var content []string
+    go read_file(file,readable,&content)
+    time.Sleep(1 * time.Millisecond)
+    select {
+        case is_readable:=<-readable:
 
-    buffered_reader:=bufio.NewReader(file)
+            if is_readable == true { is_completed:=<-readable ; if is_completed==false { defer file.Close()  } }
 
-    eof := false
-
-    mng:=make
-
-
-    for lino := 1; !eof; lino++ {
-        fmt.Println("for phase")
-
-        select{
-
-
-        case line, err := buffered_reader.ReadString('\n'):
-            fmt.Println("--")
-            fmt.Println(line)
-            fmt.Println("--")
-            if err == io.EOF {
-
-                err = nil
-                eof = true
-
-             } else if err != nil {
         default:
-             
 
-            return
+            fmt.Println("file is not readable")
+            file.Close()
 
-        }
     }
-//content, _ := ioutil.ReadFile(filename)
-//fmt.Println(content)
+    for i:=range content {
+
+        fmt.Printf("%s",content[i])
+
+    }
 }
 
-func timer(mng chan bool )(){
 
-    time.Sleep(1000 * time.Millisecond)
-    mng <- true
 
+func read_file( file *os.File, readable chan<- bool, content *[]string )(err error){
+
+    buffered_reader:=bufio.NewReader(file)
+    eof := false
+    for lino := 1; !eof; lino++ {
+        if lino ==2 {  readable<-true }
+        line, err := buffered_reader.ReadString('\n')
+        *content=append(*content,line)
+
+            if err == io.EOF {
+                err = nil
+                eof = true
+             } else if err != nil {
+            readable<-false
+            return err
+        }
+    }
+
+    readable<-false
+    return nil
 }
