e988095381b4307c282dc64ab481b2b6aaf92961
Marat <tourervs@gmail.com>
2015-09-30 11:23:51 +0300

    30092015-1123

--- tests/test_0010_dir_mtime.go
+++ tests/test_0010_dir_mtime.go
@@ -1,20 +1,35 @@
 package main
 import ("os" ; "fmt")
 import "reflect"
+import "syscall"
+
+
 
 func main() {
 
-    fi, _:=os.Stat("/tmp/test.txt")
+
+    fi, err:=os.Stat("/tmp/test1111.txt")
+
+    if err!=nil {return}
 
     mtime:=fi.ModTime()
 
     test:=fi.Sys()
 
+    test2,found :=test.(*syscall.Stat_t)
+
+    if found==false { fmt.Println("error") ; return }
+
+    fmt.Println(test2.Ino)
+
     fmt.Println(reflect.TypeOf(test))
 
     val := reflect.Indirect(reflect.ValueOf(test))
 
-    fmt.Println(val.Type().Field(1).Name)
+    //value:=test.Value
+
+    fmt.Printf("Second interface field : %s ",val.Type().Field(1).Name)
+
 
     //fmt.Println(test.Ino)
 
@@ -22,8 +37,5 @@ func main() {
 
     fmt.Println("---")
 
-    fmt.Println(test)
-
-    fmt.Println(test.Ino)
 
 }
