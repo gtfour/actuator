6167bb102035f4ac1d3c2c1ccb9561f5bbbabaf3
root <root@zombie.(none)>
2015-10-04 22:49:34 +0300

    03102015-2347

---
+++ wengine_parts/src/wengine_parts/ws_server/ws_client.go
@@ -0,0 +1,84 @@
+package wsserver
+
+import ( "fmt"
+         "io"
+         "log"
+         "code.google.com/p/go.net/websocket"
+       )
+
+const channelBufsize = 100
+
+var maxId int = 0
+
+type Client struct {
+
+    id      int
+    ws      *websocker.Conn
+    server  *Server
+    messageChannel      chan *Message
+    doneCh  chan bool
+
+}
+
+func NewClient ( ws *websocket.Conn, server *Server ) *Client {
+
+    if ws     == nil { panic("ws cannot be nil"    ) }
+    if server == nil { panic("server cannot be nil") }
+
+    maxId ++
+
+    ch     := make( chan *Message, channelBufsize )
+    doneCh := make( chan bool )
+
+    return &Client{ maxId, ws, server, ch, doneCh }
+}
+
+func ( client *Client ) Conn() *websocket.Conn {
+    return client.ws
+}
+
+func ( client *Client ) Write ( msg *Message ) {
+
+    select {
+        case client.messageChannel <- msg.Message:
+        default:
+            client.server.Del( client )
+            fmt.Errorf("Client %d is disconnected", client.id)
+            client.server.Err(err)
+    }
+
+}
+
+func ( client *Client ) Done () {
+
+    client.doneCh <- true
+}
+
+func ( client *Client ) Listen() {
+
+    go client.listenWrite()
+    client.listenRead()
+
+}
+
+func ( client *Client ) listenWrite () {
+
+    log.Println(" Listening write to client ")
+
+    for {
+        select {
+
+        case msg:= <-client.messageChannel:
+            log.Println("Send:",msg)
+            websocket.JSON.Send( client.ws, msg )
+        case <-doneCh:
+            client.server.Del(client)
+            doneCh <- true
+            return
+        }
+    }
+}
+
+func ( client *Client ) listenRead () {
+
+    log.Println(" Listening read to client")
