6c60e9a83f29bc7354fdc3a30100986d4c789663
Marat <tourervs@gmail.com>
2015-10-05 17:03:40 +0300

    05102015-1702

---
+++ client_side/src/client_side/ws_client/client.go
@@ -0,0 +1,38 @@
+package main
+//package wsclient
+
+import (
+       "code.google.com/p/go.net/websocket"
+       "fmt"
+       "log"
+)
+
+func main() {
+
+    url    := "http://10.10.111.124"
+    origin := "ws://10.10.111.124:8080/entry"
+
+    ws,err := websocket.Dial( url, "", origin)
+
+    if err != nil {
+
+        log.Fatal(err)
+
+    }
+
+    if test,err := ws.Write([]byte(`{"author":"venom","body":"Buy cheese and bread for breakfast."}`)) ; err != nil {
+
+        fmt.Println(test)
+        log.Fatal(err)
+    }
+
+    var msg = make([]byte, 512)
+    var n int
+
+    if n, err = ws.Read(msg) ; err!= nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Received: %s.\n",msg[:n])
+
+}
