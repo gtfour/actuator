0508a084f552aa3332849f2b4459a09823aa1c4e
Marat <tourervs@gmail.com>
2015-10-09 14:01:38 +0300

    09102015-1401

--- client_side/src/client_side/chase/chase.go
+++ client_side/src/client_side/chase/chase.go
@@ -31,26 +31,27 @@ type Target struct {
 
 type TargetDir struct {
 
-    Path           string
-    Dir            string
-    OldMarker      string
-    Marker         string
-    InfoIn         chan bool
-    InfoOut        chan string
-    InfoInArray    []chan bool
-    InfoOutArray   []chan string
-    MessageChannel chan   string
+    Path                 string
+    Dir                  string
+    OldMarker            string
+    Marker               string
+    InOutChannelsCreated bool
+    InfoIn               chan bool
+    InfoOut              chan string
+    InfoInArray          []chan bool
+    InfoOutArray         []chan string
+    MessageChannel       chan   string
 
 }
 
 
-func Start (targets []string, message_channel chan string)(err error){
+func Start (targets []string, message_channel chan string , subdirs *map[string]*TargetDir )(err error){
     //request_channel:=make(chan bool)
     //response_channel:=make(chan string)
 
     message_channel<-"Starting" // message just for information
 
-    subdirs := make(map[string]*TargetDir) // make subdirs map 
+    //subdirs := make(map[string]*TargetDir) // make subdirs map 
 
     for id :=range targets {
 
@@ -69,12 +70,12 @@ func Start (targets []string, message_channel chan string)(err error){
             if err !=nil { continue } // was a return err
             //add root dir
 
-            if _, ok := subdirs[targets[id]]; ok == false { // if global subdirs map does'not contain this item  targets[id] , create and add item to subdirs
+            if _, ok := (*subdirs)[targets[id]]; ok == false { // if global subdirs map does'not contain this item  targets[id] , create and add item to subdirs
 
                 tgt_dir                := &TargetDir{}
                 tgt_dir.MessageChannel =  message_channel // bind to main info-channel
                 tgt_dir.Path           =  targets[id]
-                subdirs[targets[id]]   =  tgt_dir
+                (*subdirs)[targets[id]]   =  tgt_dir
 
             }
             // 
@@ -82,12 +83,12 @@ func Start (targets []string, message_channel chan string)(err error){
 
                 path := dir_struct.SubDirs[subname]
 
-                if _, ok := subdirs[path]; ok == false { // check global subdir map again and add each included subdir if it is not included yet 
+                if _, ok := (*subdirs)[path]; ok == false { // check global subdir map again and add each included subdir if it is not included yet 
 
                     tgt_dir                := &TargetDir{}
                     tgt_dir.MessageChannel =  message_channel
                     tgt_dir.Path           =  path
-                    subdirs[path]          =  tgt_dir
+                    (*subdirs)[path]          =  tgt_dir
 
                 //go tgt_dir.ChasingDir()  // i commented this line because it caused chasing directory before assisgning all linked channels to channel Array's
                 }
@@ -103,7 +104,7 @@ func Start (targets []string, message_channel chan string)(err error){
                 target.InfoIn          =   make(chan bool,1)
                 target.InfoOut         =   make(chan string,1)
 
-                if subdir, ok := subdirs[file_struct.Dir]; ok { // check Dir field of File struct and try to bind File channel with TargetDir channel Array 
+                if subdir, ok := (*subdirs)[file_struct.Dir]; ok { // check Dir field of File struct and try to bind File channel with TargetDir channel Array 
 
                     target.Dir          =  file_struct.Dir
                     subdir.InfoInArray  =  append(subdir.InfoInArray,target.InfoIn)
@@ -139,21 +140,25 @@ func Start (targets []string, message_channel chan string)(err error){
 
     }
     }
-    for i:=range subdirs {
+    for i:=range (*subdirs) {
 
         //message_channel <- "subdir : " +subdirs[i].Path
 
         // ебучее говно : поиск родительской директории для этой субдиректории в массиве субдиректорий 
         dir := filepath.Dir(i)
         // ааа бляять - мои мозги !!!! 
-        if parent_dir, ok := subdirs[dir]; ok {
+        if parent_dir, ok := (*subdirs)[dir]; ok {
 
-            subdirs[dir].InfoIn          =   make(chan bool,1)
-            subdirs[dir].InfoOut         =   make(chan string,1)
-            subdirs[dir].Dir             =   parent_dir.Path
+            if (!(*subdirs)[dir].InOutChannelsCreated ) {
 
-            parent_dir.InfoInArray       =  append(parent_dir.InfoInArray, subdirs[dir].InfoIn)
-            parent_dir.InfoOutArray      =  append(parent_dir.InfoOutArray, subdirs[dir].InfoOut)
+                (*subdirs)[dir].InfoIn          =   make(chan bool,1)
+                (*subdirs)[dir].InfoOut         =   make(chan string,1)
+
+            }
+            (*subdirs)[dir].Dir             =   parent_dir.Path
+
+            parent_dir.InfoInArray       =  append(parent_dir.InfoInArray, (*subdirs)[dir].InfoIn)
+            parent_dir.InfoOutArray      =  append(parent_dir.InfoOutArray, (*subdirs)[dir].InfoOut)
 
         }
 
@@ -161,14 +166,14 @@ func Start (targets []string, message_channel chan string)(err error){
 
     }
 
-    for i:=range subdirs {
+    for i:=range (*subdirs) {
 
-        message_channel <- "subdir : " +subdirs[i].Path
-        go subdirs[i].ChasingDir()
+        message_channel <- "subdir : " +(*subdirs)[i].Path
+        go (*subdirs)[i].ChasingDir()
 
     }
 
-    return nil
+    return
 }
 
 func Stop()(err error) {
@@ -255,9 +260,12 @@ func (tgt *Target) ChasingFile() (err error){
 
 func (tgt *TargetDir) ChasingDir () (err error){
 
-    tgt.MessageChannel <- "start chasing of dir : "+tgt.Path
-    tgt.MessageChannel <- "InfoInArray len : " +fmt.Sprintf("%d",len(tgt.InfoInArray))+"path : "+tgt.Path+"\n"
-    tgt.MessageChannel <- "InfoOutArray len : " +fmt.Sprintf("%d",len(tgt.InfoOutArray))+"path : "+tgt.Path+"\n"
+    tgt.MessageChannel <- ">>| Start chasing of dir : "+tgt.Path
+
+    tgt.MessageChannel <- ">>| InfoInArray len : " +fmt.Sprintf("%d",len(tgt.InfoInArray))+"path : "+tgt.Path+"\n"
+
+    tgt.MessageChannel <- ">>| InfoOutArray len : " +fmt.Sprintf("%d",len(tgt.InfoOutArray))+"path : "+tgt.Path+"\n"
+
    //dup
     for {
         var inform_about_exit bool
@@ -285,7 +293,16 @@ func (tgt *TargetDir) ChasingDir () (err error){
 
             var new_items = []string { tgt.Path }
 
-            go Start( new_items, tgt.MessageChannel )
+            subdirs                      := make(map[string]*TargetDir)
+            tgt_new                      := &TargetDir{}
+            tgt_new.MessageChannel       =  tgt.MessageChannel
+            tgt_new.Path                 =  tgt.Path
+            tgt_new.InfoIn               =  tgt.InfoIn
+            tgt_new.InfoOut              =  tgt.InfoOut
+            tgt_new.InOutChannelsCreated = true
+            subdirs[tgt.Path]            =  tgt_new
+
+            go Start( new_items, tgt.MessageChannel , &subdirs )
 
             return nil
 
@@ -341,9 +358,18 @@ func (tgt *Target) Reporting () {
 
 func Listen() (messages chan string){
 
-    messages      =  make(chan string,100)
-    var test_dir  =  []string { "/tmp/test" }
-    Start( test_dir, messages )
+    messages                    =  make(chan string,100)
+    var test_dir                =  []string { "/tmp/test" }
+    target                      :=  &TargetDir{}
+    target.Path                 =  "/tmp/test"
+    target.InfoIn               =  make(chan bool,1)
+    target.InfoOut              =  make(chan string,1)
+    target.InOutChannelsCreated =  true
+    subdirs                     := make(map[string]*TargetDir)
+    subdirs[target.Path]        =  target
+
+
+    Start( test_dir, messages, &subdirs )
     return
 
 }
