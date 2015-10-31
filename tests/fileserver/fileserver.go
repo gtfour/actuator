a22d4944181a6d2e0202cbe35949f8a0b39ce0b6
root <root@zombie.(none)>
2015-10-31 19:23:52 +0300

    30102015-20:22 . I have good news: fixed error : panic: runtime error: invalid memory address or nil pointer dereference . I so fucking happy . Seems that i had to add WP pointer to each tgt_dir and target .  But now Chase() functions array is increasing dynamically . Have to fix it

--- tests/fileserver/fileserver.go
+++ tests/fileserver/fileserver.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	// Simple static webserver:
 	//log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/home/venom/gopro/repo_data/rpm/"))))
-        log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/tmp/static_test1"))))
+        log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/static"))))
         //log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/tmp/static_test2"))))
 }
