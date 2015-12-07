8ba07b62d31b73e66b90f2d255a693b07d698d11
root <you@example.com>
2015-12-07 18:08:07 +0400

    cuda::analyze

--- client_side/src/client_side/cuda/analyze.go
+++ client_side/src/client_side/cuda/analyze.go
@@ -1,7 +1,54 @@
 package cuda
 
+/*
 var splitted_by_space  int = 0
 var splitted_by_colon  int = 1
+*/
+
+var OPEN_SECTION_SQUARE      int = 0   //   [
+var CLOSE_SECTION_SQUARE     int = 1   //   ]
+var OPEN_SECTION_TRIANGLE    int = 2   //   <
+var OPEN_SL_SECTION_TRIANGLE int = 3   //   </
+var CLOSE_SECTION_TRIANGLE   int = 4   //   >
+var OPEN_SECTION_ROUND       int = 5   //   (
+var CLOSE_SECTION_ROUND      int = 6   //   )
+
+var SINGLE_QUOTE             int = 7   //   '
+var DOUBLE_QUOTE             int = 8   //   "
+var GRAVE_QUOTE              int = 9   //   `
+
+var HYPHEN                   int = 10  // -
+var MINUS                    int = 10  // -
+var PLUS                     int = 11
+var UNDERSCORE               int = 12  // _
+var EQUAL                    int = 13  // = 
+var COLON                    int = 14  // : 
+var SEMICOLON                int = 15  // ;
+var COMMA                    int = 16  // ,
+var DOT                      int = 17
+
+var SLASH                    int = 18  // /
+var BACKSLASH                int = 19  // \
+var PIPE                     int = 20  // |
+var ASTERISK                 int = 21  // *
+var NUMBER                   int = 22  // #
+var PENIS                    int = 23  // o|o
+var DOLLAR                   int = 24  // $
+var AMPERSAND                int = 25  // &
+var SUFFIX                   int = 26  // ^
+var PERCENT                  int = 27  // %
+var MAIL                     int = 28  // @
+var EXCLAM                   int = 29  // !
+var TILDE                    int = 30  // ~
+
+
+
+
+
+
+
+
+//var SQ_CU
 
 
 
@@ -19,3 +66,15 @@ func AcidSequencer(entry string) ()  {
 }
 
 //func 
+
+/*
+
+var SECTION_SQUARE_OPEN = int  0 
+
+
+
+
+
+
+
+*/
