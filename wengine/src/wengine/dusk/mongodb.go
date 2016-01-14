d8725289bb9a835b571e66ae41f3b6322531e2a6
root <you@example.com>
2016-01-14 14:23:50 +0400

    wengine:db

--- wengine/src/wengine/dusk/mongodb.go
+++ wengine/src/wengine/dusk/mongodb.go
@@ -1,3 +1,12 @@
 package dusk
+import "gopkg.in/mgo.v2"
+//import "gopkg.in/mgo.v2/bson"
 
-func Dial(username string, password string, url string, dbname string ) ()
+func CreateMongoDBConn( username string, password string, host string, dbname string ) ( session *mgo.Session, err error  ) {
+
+    return mgo.Dial("mongodb://"+username+":"+password+"@"+host+"/"+dbname)
+
+
+}
+
+func 
