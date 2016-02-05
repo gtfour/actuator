6f63239eb8f53e2f50d247fa82fe700ac6ebfdd5
root <you@example.com>
2016-02-05 20:32:34 +0400

    interesting progress

--- wapour/src/wapour/userspace/navigation.go
+++ wapour/src/wapour/userspace/navigation.go
@@ -15,20 +15,30 @@ type Item struct {
 
 func CreateNavigationMenu(dinstance webclient.DashboardListResult)( []Item ){
 
-    dgroup_list:= make([]Item,0)
+    dgroup_list        := make([]Item,0)
+    //default_item       := Item{Name:"Default",Id:"default_dashboard",Icon:"fa-cubes"}
+    //var grouped_dashboards  []string
 
     for dgroup_id := range dinstance.DashboardGroupList.List {
         dgroup    := dinstance.DashboardGroupList.List[dgroup_id]
         //dgroup    := dinstance.DashboardGroupList[dgroup_id]
-        item      := Item{Name:dgroup.Title, Id:dgroup.Id, Icon:dgroup.Icon}
+        item      := Item{ Name:dgroup.Title, Id:dgroup.Id, Icon:dgroup.Icon }
         for dashboard_id := range dinstance.DashboardList.List {
             dashboard := dinstance.DashboardList.List[dashboard_id]
-            subitem:= SubItem{Name:dashboard.Name}
-            item.SubItems = append(item.SubItems, subitem)
+            if IsIn(dashboard.Id, dgroup.List){
+                subitem:= SubItem{Name:dashboard.Title}
+                item.SubItems = append(item.SubItems, subitem)
+            }
         }
         dgroup_list = append(dgroup_list, item)
     }
     return dgroup_list
 }
 
-
+func IsIn(x string,xs []string)(bool) {
+    for i := range xs {
+        y:= xs[i]
+        if y == x {return true}
+    }
+    return false
+}
