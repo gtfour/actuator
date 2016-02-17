b9a98c861a5b8f3f77f39265249700f35190644a
root <you@example.com>
2016-02-17 17:16:47 +0400

    fucking amazing ls

--- client/src/client/chase/tm.go
+++ client/src/client/chase/tm.go
@@ -11,7 +11,7 @@ import "client/evebridge"
 
 var TGT_PER_GR int64                       = 50 // if FILES_PER_GR is very big - TargetsCount type should be modified 
 var TIMEOUT_MS              time.Duration  = 800
-var INHIBITION_TIMEOUT      time.Duration  = 3000
+var INHIBITION_TIMEOUT      time.Duration  = 1000
 // time.Sleep( INHIBITION_TIMEOUT * time.Millisecond)
 var LOG_CHANNEL_TIMEOUT_MS  time.Duration  = 1000
 
