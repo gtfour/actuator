7cc7cc7734b29bd20c00af6cfee9918b587c5215
root <you@example.com>
2016-02-25 15:09:13 +0400

    Checking symbol is letter

---
+++ tests/test_0049_issymbol.go
@@ -0,0 +1,25 @@
+package main
+import . "unicode"
+import "fmt"
+
+func main() {
+
+    mystring     := "a"
+    myunderscore := "_"
+    mydigit      := "1"
+    var mybyte1,mybyte2,mybyte3 byte
+    if len(mystring) == 1 {
+        mybyte1=mystring[0]
+    }
+    if len(myunderscore) == 1 {
+        mybyte2=myunderscore[0]
+    }
+    if len(mydigit) == 1 {
+        mybyte3=mydigit[0]
+    }
+
+    fmt.Printf("%v   %v   %v\n",IsLetter(rune(mybyte1)),IsLetter(rune(mybyte2)),IsLetter(rune(mybyte3)))
+    for _,r := range "a"  { // knows about russian letters
+        fmt.Printf("%v\n",IsLetter(r))
+    }
+}
