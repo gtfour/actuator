b31a72d2d3cfc5388c6e3e2c410acd5e437038e8
root <you@example.com>
2016-03-08 19:24:24 +0400

    simple

--- client/src/client/cuda/resolver.go
+++ client/src/client/cuda/resolver.go
@@ -1,4 +1,5 @@
 package cuda
+import "fmt"
 
 type Cyclone struct {
     // line prop
@@ -8,8 +9,24 @@ func DataHeaderSelector(first_table_string []string)(data [][]int, isTableHeader
     return
 }
 
-func UrlSelector(str []string)(data [][]int, isUrl bool ) {
+func UrlSelector(str []string, delim []int,  data_before []int , data_after []int)(data [][]int, isUrl bool ) {
+    fmt.Printf("Delim:%v StrPart:%v", delim,str[delim[0]:delim[1]])
     return
+}
+
+func UrlMatcher(str []string, delim []int ) {
+
+    match:=str[delim[0]:delim[1]]
+    fmt.Printf("match:%v  str:%v  delim:%v",match,str,delim )
+
+
+}
+
+func StringArrayIsEqual (abc , def []string) (bool) {
+
+
+
+
 }
 
 //func AnalyzeDelims()
