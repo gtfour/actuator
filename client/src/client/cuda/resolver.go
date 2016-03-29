6fdcfb1c7be69ab778109f2cdce3e39a36af8656
root <you@example.com>
2016-03-29 18:04:54 +0400

    new return

--- client/src/client/cuda/resolver.go
+++ client/src/client/cuda/resolver.go
@@ -63,9 +63,10 @@ func StringArrayIsEqual (abc , def []string) (bool) {
 
 func BracketsFilter(lineAsArray []string , delims [][]int , data [][]int)(ndelims [][]int , ndata [][]int) {
 
-    var section_brackets_square   =  [2]string {"[","]"}
-    var section_brackets_triangle =  [3]string {"<",">","</"}
-    var section_brackets_curly    =  [2]string {"{","}"}
+    //var section_brackets_square   =  [2]string {"[","]"}
+    //var section_brackets_triangle =  [3]string {"<",">","</"}
+    //var section_brackets_curly    =  [2]string {"{","}"}
+    return
 
 }
 
