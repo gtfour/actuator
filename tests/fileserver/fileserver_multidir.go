7509920c1b1b90ea73ed5acb08324d333aa07b20
root <you@example.com>
2016-03-30 20:27:05 +0400

    web tests dir

---
+++ tests/fileserver/fileserver_multidir.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+        //log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/adminlte/AdminLTE"))))
+        //log.Fatal(http.ListenAndServe(":8080", nil))
+        //log.Fatal(http.ListenAndServe(":8080/angular", http.FileServer(http.Dir("/actuator/tests/web_tests"))))
+        http.Handle("/angular/", http.StripPrefix("/angular/", http.FileServer(http.Dir("/actuator/tests/web_tests"))))
+        http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("/adminlte/AdminLTE"))))
+        log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
---
+++ tests/web_tests/angular_service/app.js
@@ -0,0 +1,26 @@
+var testApp = angular.module('testApp',[]);
+
+testApp.factory('dataService', function(){
+    return {messages:["Hello all!","Buy!","Nice to see you!"],
+            data:function get_table_data(table_id){} ,};
+});
+
+testApp.controller('ResponseController', function ResponseController($scope, dataService) {
+    $scope.answers = dataService.messages ; 
+    $scope.word = "Habrahabr";
+}); 
+
+testApp.directive('habraHabr', function () {
+
+    return function(scope,element,attrs) {
+        scope.$watch(attrs.habraHabr, function(value) {
+            element.text(value+attrs.habr);
+        });
+        scope.$watch(attrs.habraHabr, function(value) {
+            //element.text(value+attrs.habr);
+            alert(value);
+        });
+        }
+    });
+
+
