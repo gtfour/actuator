bdba0a240f2bdc6607ff9ed9ef454e96eb910404
root <root@zombie.(none)>
2016-04-04 00:10:29 +0300

    ng tests

--- tests/fileserver/fileserver_multidir.go
+++ tests/fileserver/fileserver_multidir.go
@@ -11,7 +11,7 @@ func main() {
         //log.Fatal(http.ListenAndServe(":8080", nil))
         //log.Fatal(http.ListenAndServe(":8080/angular", http.FileServer(http.Dir("/actuator/tests/web_tests"))))
         http.Handle("/angular/", http.StripPrefix("/angular/", http.FileServer(http.Dir("/actuator/tests/web_tests"))))
-        http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("/adminlte/AdminLTE"))))
+        http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("/adminlte"))))
         log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
