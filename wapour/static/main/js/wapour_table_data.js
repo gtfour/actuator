fa75a5104162663dbd77eebe00eae9efa4bd7b64
root <you@example.com>
2016-04-04 20:22:26 +0400

    simple fix

--- wapour/static/main/js/wapour_table_data.js
+++ wapour/static/main/js/wapour_table_data.js
@@ -51,3 +51,14 @@ wapourApp.directive('wapourInfoBox', ['websocketService',function (websocketServ
         }
     }
 }]);
+
+/*
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-more">Likes</span>
+            <span class="info-box-number">41,410</span>
+            <span class="info-box-more" style="font-size:8px;">4E4BFE80-6FCC-B5E9-1AE6-DA906A88BD3B</span>
+            <a href="www.google.com"  class="info-box-more" style="font-size:8px;">More Info</a>
+          </div>
+        </div>
+*/
