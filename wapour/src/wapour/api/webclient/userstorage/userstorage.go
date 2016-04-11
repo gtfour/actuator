0ef186cc9b91b4cf02de532000cc4d6c653d3fb5
root <you@example.com>
2016-04-11 17:19:12 +0400

    extending webclient

---
+++ wapour/src/wapour/api/webclient/userstorage/userstorage.go
@@ -0,0 +1,38 @@
+package userstorage
+
+import "wapour/settings"
+import "wapour/api/webclient"
+import "github.com/boltdb/bolt"
+
+var UsersStorage webclient.WengineWrapperStorage
+
+type FileUserStorage struct {
+    StorageType      string
+    Wrappers         *[]*webclient.WengineWrapper
+}
+
+type BoltdbUserStorage   struct {
+   StorageType      string
+   StorageFileName  string
+   Db bolt.DB
+}
+
+func init() {
+    var db_open_failed bool
+    if settings.ONLINE_USERS_STORAGE_TYPE        == "db" {
+        UsersStorage.StorageType = "db"
+        db_path                  := settings.ONLINE_USERS_DB_FILE
+        db, err                  := bolt.Open(db_path, 0600, nil)
+        if err!= nil {
+            defer db.Close()
+            db_open_failed = true
+        }
+    } else if settings.ONLINE_USERS_STORAGE_TYPE == "ram" || db_open_failed == true {
+        UsersStorage.StorageType = "ram"
+    }
+}
+
+func main() {
+
+}
+
