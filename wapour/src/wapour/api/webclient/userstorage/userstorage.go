c82be2693fb9f0022243095cca436c6c7785d1e1
root <you@example.com>
2016-04-11 20:22:34 +0400

    fix

--- wapour/src/wapour/api/webclient/userstorage/userstorage.go
+++ wapour/src/wapour/api/webclient/userstorage/userstorage.go
@@ -4,15 +4,12 @@ import "wapour/settings"
 import "wapour/api/webclient"
 import "github.com/boltdb/bolt"
 
-var UsersStorage webclient.WengineWrapperStorage
+//var UsersStorage webclient.WengineWrapperStorage
+var UserStorageInstance UserStorage
 
 type UserStorage interface {
-
-    FindWrapper(err error)
-    AddWrapper()
-
-
-
+    FindWrapper(user_id string, token_id string)(err error)
+    AddWrapper(w *webclient.WengineWrapper)(err error)
 }
 
 
@@ -24,18 +21,22 @@ type FileUserStorage struct {
 type BoltdbUserStorage   struct {
    StorageType      string
    StorageFileName  string
+   DbName           string
    Db bolt.DB
 }
 
 func init() {
     var db_open_failed bool
     if settings.ONLINE_USERS_STORAGE_TYPE        == "db" {
-        UsersStorage.StorageType = "db"
+        //UsersStorage.StorageType = "db"
         db_path                  := settings.ONLINE_USERS_DB_FILE
         db, err                  := bolt.Open(db_path, 0600, nil)
         if err!= nil {
             defer db.Close()
             db_open_failed = true
+        } else {
+
+
         }
     } else if settings.ONLINE_USERS_STORAGE_TYPE == "ram" || db_open_failed == true {
         UsersStorage.StorageType = "ram"
@@ -44,5 +45,11 @@ func init() {
 
 func main() {
 
+}
+
+func (boltdb *BoltdbUserStorage) FindWrapper (user_id string, token_id string) (err error) {
+
+
+    return err
 }
 
