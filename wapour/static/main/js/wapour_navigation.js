ac04d55c12fe88668d1ea5518d177ac221734467
root <you@example.com>
2016-05-04 15:07:57 +0400

    fix

--- wapour/static/main/js/wapour_navigation.js
+++ wapour/static/main/js/wapour_navigation.js
@@ -4,6 +4,20 @@ wapourApp.directive('navigationItem', ['dashboardDataService',function (dashboar
         link:     function(scope, elem, attrs) {
             elem.bind('click', function() {
                 dashboard_group_id = attrs.dashboardGroupId;
+                dashboard_id       = attrs.dashboardId ;
+                dashboardDataService.SelectDashboard(dashboard_id, dashboard_group_id);
+        });
+        }
+    }
+}]);
+
+
+wapourApp.directive('navigationSubitem', ['dashboardDataService',function (dashboardDataService) {
+    return {
+        restrict: 'AE',
+        link:     function(scope, elem, attrs) {
+            elem.bind('click', function() {
+                dashboard_group_id = attrs.dashboardGroupId;
                 dashboard_id       = attrs.dashboardId ; 
                 dashboardDataService.SelectDashboard(dashboard_id, dashboard_group_id);
         });
