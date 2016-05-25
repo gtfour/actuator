36f00f8b0e3d4c543efafb5c4a4ff376a8f204f9
root <you@example.com>
2016-05-25 12:26:02 +0400

    fix

--- tests/test_0058_gen_template.go
+++ tests/test_0058_gen_template.go
@@ -19,9 +19,10 @@ func main() {
 
 
   //new_data    := []int {1,2,3}
-  line        := "a=22 b=1 c=0"
+  //line        := "a=22 b=1 c=0"
+  line        := "a='' b=1 c=0"
   lineAsArray := strings.Split(line, "")
-  data_pos    := [][]int{{2,3},{7,7},{11,11}}
+  data_pos    := [][]int{{3,2},{7,7},{11,11}}
   //fmt.Printf("\n %v   %v\n",new_data,line)
 
   GenTemplate(lineAsArray, data_pos)
@@ -53,17 +54,34 @@ func GetFixedArrayChars(lineAsArray []string, data_indexes[][]int) (string) {
     word:=""
     template_variable_counter:=0
 
-    for i := range  lineAsArray {
-        last_pair_index:=-1
-        on_interval:=false
+    //for i := range  lineAsArray {
+    for i := 0; i < len(lineAsArray); i++ {
+        //last_pair_index := -1
+        on_interval     := false
+        invert          := false
+        matched_pair    := []int{-1,-1}
         for z := range data_indexes {
             pair:=data_indexes[z]
-            if DigitInInterval(i, pair) == DIGIT_IN_INTERVAL {
-                on_interval = true
-                if len(pair) == 2 {
-                    last_pair_index = pair[1]
+            if (len(pair)==2 && pair[0]<=pair[1]) {
+                if DigitInInterval(i, pair) == DIGIT_IN_INTERVAL {
+                    on_interval = true
+                    if len(pair) == 2 {
+                        //last_pair_index = pair[1]
+                        matched_pair = pair
+                    }
+                    break
                 }
-                break
+            } else {
+                temp_pair := []int{pair[1],pair[0]}
+                    if DigitInInterval(i, temp_pair) == DIGIT_IN_INTERVAL {
+                        on_interval = true
+                        invert      =  true
+                        if len(pair) == 2 {
+                            //last_pair_index = pair[1]
+                            matched_pair = temp_pair
+                        }
+                        break
+                    }
             }
         }
 
@@ -71,9 +89,16 @@ func GetFixedArrayChars(lineAsArray []string, data_indexes[][]int) (string) {
         if (on_interval == false) {
             word=word+lineAsArray[i]
         } else {
-            word=word+fmt.Sprintf("{{index .Data %d}}",template_variable_counter)
+            if ( invert==false ) {
+                word=word+fmt.Sprintf("{{index .Data %d}}",template_variable_counter)
+            } else {
+                first_delim:=matched_pair[0]
+                last_delim :=matched_pair[1]
+                word=word+fmt.Sprintf(lineAsArray[first_delim]+"{{index .Data %d}}"+lineAsArray[last_delim],template_variable_counter)
+
+            }
             template_variable_counter+=1
-            i = last_pair_index
+            i = matched_pair[1]// start iterating array from next index of found pair
         }
     }
     fmt.Printf("\nword %s\n",word)
