b084832bc4be77d77bb21e33816682f3c3c10433
root <you@example.com>
2016-05-25 14:11:58 +0400

    fix

--- tests/test_0058_gen_template.go
+++ tests/test_0058_gen_template.go
@@ -11,18 +11,17 @@ var DIGIT_GREATER_INTERVAL int = 9753
 var DIGIT_IN_INTERVAL      int = 9779
 
 type Tango struct {
-    Data []int
+    Data []string
 }
 
 
 func main() {
 
-
   //new_data    := []int {1,2,3}
   //line        := "a=22 b=1 c=0"
-  line        := "a='' b=1 c=0"
+  line        := "наименование='курица' вес=3.5кг процентное содержание воды=40%"
   lineAsArray := strings.Split(line, "")
-  data_pos    := [][]int{{3,2},{7,7},{11,11}}
+  data_pos    := [][]int{{14,19},{26,28},{59,60}}
   //fmt.Printf("\n %v   %v\n",new_data,line)
 
   GenTemplate(lineAsArray, data_pos)
@@ -33,7 +32,7 @@ func main() {
 //func GenTemplate(lineAsArray []string, data_pos [][]int)( *template.Template) {
 func GenTemplate(lineAsArray []string, data_pos [][]int)() {
     //new_line = ``
-    new_data    := []int {1,2,3}
+    new_data    := []string {"говядина","1.3","30"}
 
     template_string:=GetFixedArrayChars(lineAsArray, data_pos)
 
@@ -53,10 +52,14 @@ func GetFixedArrayChars(lineAsArray []string, data_indexes[][]int) (string) {
 
     word:=""
     template_variable_counter:=0
+    fmt.Printf("\n Line Array %v\n",lineAsArray)
 
     //for i := range  lineAsArray {
     for i := 0; i < len(lineAsArray); i++ {
+    //for i,c := range  lineAsArray {
         //last_pair_index := -1
+        //if i == start_at {
+        //fmt.Printf("%s %s  %d\n",lineAsArray[i],string(c),i)
         on_interval     := false
         invert          := false
         matched_pair    := []int{-1,-1}
@@ -95,7 +98,6 @@ func GetFixedArrayChars(lineAsArray []string, data_indexes[][]int) (string) {
                 first_delim:=matched_pair[0]
                 last_delim :=matched_pair[1]
                 word=word+fmt.Sprintf(lineAsArray[first_delim]+"{{index .Data %d}}"+lineAsArray[last_delim],template_variable_counter)
-
             }
             template_variable_counter+=1
             i = matched_pair[1]// start iterating array from next index of found pair
