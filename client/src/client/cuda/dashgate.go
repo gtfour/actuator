6e9ebee68d2c7de4f2b0881dda4d04185ba37289
root <you@example.com>
2016-05-31 17:23:00 +0400

    fix

---
+++ client/src/client/cuda/dashgate.go
@@ -0,0 +1,36 @@
+package cuda
+
+var DASHGATE_TYPE_SINGLE      int = 10000
+var DASHGATE_TYPE_SINGLE_FILE int = 10001
+var DASHGATE_TYPE_GROUP       int = 10002
+
+type Dashgate struct {
+    Id               string
+    SourcePath       string
+    SourceType       string // file or command
+    //
+    //
+    FileBaseDir      string
+    Dynimas          []*Dynima
+    DefaultDynima    *Dynima
+    FileExtension    string
+}
+
+func (d *Dashgate)PassDataToDynima(dynima_id string)(error){
+
+    return nil
+
+}
+
+
+func (d *Dashgate)EditDefaultDynima(dynima_id string)(error){
+
+    return nil
+
+}
+
+func (d *Dashgate)Update()(error) {
+
+    return nil
+
+}
