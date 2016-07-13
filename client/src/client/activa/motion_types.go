4224645526c29da425c231d0192ffe98a5d4704a
root <you@example.com>
2016-07-13 15:45:00 +0400

    hello

--- client/src/client/activa/motion_types.go
+++ client/src/client/activa/motion_types.go
@@ -1,5 +1,8 @@
 package activa
 
-var MOTION_TYPE_BLACKOUT               int = 8016
-var MOTION_TYPE_BLACKTOP               int = 8061
+var MOTION_TYPE_BLACKOUT       int = 8016
+var MOTION_TYPE_BLACKOUT_LOCAL int = 8017
+
+var MOTION_TYPE_BLACKTOP       int = 8061
+var MOTION_TYPE_BLACKTOP_LOCAL int = 8062
 
--- client/src/client/riva/riva.go
+++ client/src/client/riva/riva.go
@@ -1,16 +1,62 @@
 package riva
 
+import "client/activa"
+//import "client/evebridge"
+
+func Handle()(error){
+
+    return nil
+
+
+}
+
+
+
 type Trigger interface {
-    Meow()
+
+    SetMeow(*Meow)
+    GetMeow(meow_id string)
+    SetFractal(*Fractal)
+    GetFractal(fractal_id string)
+    Remove()
+
 }
 
-type Condition struct {
+type Meow interface {
 
+    Set()
+    Write()
+    Remove()
+
+}
+
+type Fractal interface {
+
+    Set()
+    Run()
+    Remove()
+
+}
+
+type Runner struct {
 
 
 }
 
-type Mads struct {
+type Editor struct {
 
 
+}
+
+func CreateTrigger()(t *Trigger,err error) {
+
+
+
+    return
+}
+
+func MakeShiver()(m *activa.Motion){
+
+
+    return
 }
