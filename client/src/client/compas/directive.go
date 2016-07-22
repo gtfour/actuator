95c7dce403ea2e3e336fb0e850d55a443bb01985
root <you@example.com>
2016-07-22 17:26:23 +0400

    hello

--- client/src/client/compas/directive.go
+++ client/src/client/compas/directive.go
@@ -1,6 +1,14 @@
 package compas
 
+
 var INSERT        int  = 4000
 var INSERT_AFTER  int  = 4001
 var INSERT_BEFORE int  = 4002
 
+var REPLACE_WITH  int  = 4050
+
+var REMOVE        int  = 4100
+var REMOVE_AFTER  int  = 4101
+var REMOVE_BEFORE int  = 4102
+
+var APPEND        int  = 4200
