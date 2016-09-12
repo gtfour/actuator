0c430dc6ebff34211b6b7dc2f43595da5f686104
root <root@zombie.(none)>
2016-09-12 14:47:05 +0300

    highload test

---
+++ tests/test_hl_dup/common.go
@@ -0,0 +1,15 @@
+package main
+
+type MyFile struct {
+
+
+
+}
+
+func OpenFile(path string)(error){
+    return nil
+}
+
+func (file *MyFile)AddEntry()(){
+
+}
---
+++ tests/test_hl_dup/main.go
@@ -0,0 +1,12 @@
+package main
+import   "github.com/gin-gonic/gin"
+
+var LOG_FILE string = "/tmp/connection.log"
+
+func main(){
+    app      := gin.Default()
+    //app.POST("/check", Check())
+    app.GET("/check", Check())
+    app.Run("127.0.0.1:9020")
+
+}
