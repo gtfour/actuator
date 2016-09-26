2e155db7a8f74f749f26f6bbc2d21a7d16464f5d
root <you@example.com>
2016-09-26 19:37:09 +0400

    hello

--- wapour/src/wapour/core/run/default.go
+++ wapour/src/wapour/core/run/default.go
@@ -1,1 +1,16 @@
 package run
+
+var default_props = GetDefaultProp()
+
+func SetDefaultProp()(prop map[string]string){
+
+    prop=make(map[string]string)
+    return
+
+}
+
+func GetDefaultPropvalue(key string)(value string, err error){
+
+
+}
+
