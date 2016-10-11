54981b626bb714dd26a09b2a481a6ec2e364d1f9
root <you@example.com>
2016-10-11 20:37:32 +0400

    hello

--- jumper/src/jumper/petrovich/get_config.go
+++ jumper/src/jumper/petrovich/get_config.go
@@ -2,10 +2,18 @@ package petrovich
 
 import "jumper/common/file"
 
-func GetConfigFromFile(filename string)(config map[string]string,err error){
+func LoadConfig(filename string, initial_config ...map[string]string)(config map[string]string,err error){
     lines,err:=file.ReadFile(filename)
     if err!= nil {
         return nil,err
     }
+    var parser huyamba
+    if len(initial_config)>0{
+        parser=CreateHuyamba(initial_config[0])
+    } else {
+        parser=CreateHuyamba()
+    }
+
+
     return config, nil
 }
---
+++ jumper/src/jumper/petrovich/precision_beast.go
@@ -0,0 +1,16 @@
+package petrovich
+
+type huyamba struct {
+
+    initial map[string]string
+
+
+}
+
+
+func CreateHuyamba(initial_config ...map[string]string)(parser huyamba){
+    if len(initial_config)>0 {
+
+    }
+    return parser 
+}
