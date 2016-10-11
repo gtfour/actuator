134a57288063d6c52f16b28bd8a7cf34d469c203
root <you@example.com>
2016-10-11 20:52:34 +0400

    hello

--- jumper/src/jumper/petrovich/precision_beast.go
+++ jumper/src/jumper/petrovich/precision_beast.go
@@ -1,16 +1,15 @@
 package petrovich
 
 type huyamba struct {
-
-    initial map[string]string
-
-
+    initial_config map[string]string
+    self_config    map[string]string
 }
 
-
 func CreateHuyamba(initial_config ...map[string]string)(parser huyamba){
-    if len(initial_config)>0 {
+    if len(initial_config)>0{
+        for key := range initial_config {
+        }
 
     }
-    return parser 
+    return parser
 }
