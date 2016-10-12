8232a4c3fdcc37f71a34ea0f3c507703cf8225c9
root <you@example.com>
2016-10-12 17:11:03 +0400

    hello

---
+++ jumper/src/jumper/petrovich/errors.go
@@ -0,0 +1,4 @@
+package petrovich
+
+var VALUE_EXIST     int = 10000
+var VALUE_NOT_FOUND int = 10001
--- jumper/src/jumper/petrovich/precision_beast.go
+++ jumper/src/jumper/petrovich/precision_beast.go
@@ -1,15 +1,23 @@
 package petrovich
 
 type huyamba struct {
-    initial_config map[string]string
-    self_config    map[string]string
+    default_config  map[string]string
+    cmdline_config  map[string]string
+    self_config     map[string]string
+}
+
+func(h *huyamba)GetValue(key string)(value string,err int){
+
+    return value,err
 }
 
 func CreateHuyamba(initial_config ...map[string]string)(parser huyamba){
+    parser.initial_config = make(map[string]string,0)
     if len(initial_config)>0{
-        for key := range initial_config {
+        first_initial_config:=initial_config[0]
+        for key,value := range first_initial_config {
+            parser.initial_config[key] = value
         }
-
     }
     return parser
 }
