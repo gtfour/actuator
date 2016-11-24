4afef764123a2d12001bb25e32942b1b07e7ddb9
gtfour <gtfour@gmail.com>
2016-11-24 21:56:58 +0300

    hello

--- jumper/src/jumper/system/system.go
+++ jumper/src/jumper/system/system.go
@@ -1,4 +1,9 @@
 package system
 
-var TYPE_FILE int = 3000
-var TYPE_DIR  int = 3002
+var TYPE_FILE      int = 3000
+var TYPE_DIR       int = 3002
+var TYPE_UNDEFINED int = 3005
+
+func GetFileType(path string)(int){
+    return TYPE_UNDEFINED
+}
