bd867d8530f9dc1c63d0b013c666e5d17f84cb64
gtfour <gtfour@gmail.com>
2016-11-26 21:53:32 +0300

    hello

--- jumper/src/jumper/system/system.go
+++ jumper/src/jumper/system/system.go
@@ -1,9 +1,13 @@
 package system
 
-var TYPE_FILE      int = 3000
-var TYPE_DIR       int = 3002
-var TYPE_UNDEFINED int = 3005
+import "os"
+
+var TYPE_FILE           int = 3000
+var TYPE_DIR            int = 3002
+var UNABLE_TO_OPEN_FILE int = 3005
+var TYPE_UNDEFINED      int = 3007
 
 func GetFileType(path string)(int){
+    f,err := os.Open(path)
     return TYPE_UNDEFINED
 }
