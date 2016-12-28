12d9768a5375ae16fb9ac2f2e7197ec742b71ec4
gtfour <gtfour@gmail.com>
2016-12-28 13:06:19 +0300

    hello

--- jumper/src/jumper/cross/client/client.go
+++ jumper/src/jumper/cross/client/client.go
@@ -3,12 +3,8 @@ package client
 import "jumper/cross"
 
 func CreateConnector(dbtype string)(*cross.Database,error){
-    connectorTemplate,err:=cross.CreateConnectorTemplate(dbtype)
-    if err!=nil {return nil,err}
+    connectorTemplate,err:=cross.CreateConnectorTemplate( dbtype )
+    if err!=nil { return nil,err }
     if !cross.IsOk(connectorTemplate.Dbtype, valid_db_types) {return nil, cross.Selected_dbtype_is_not_ok_on_client_side  }
     return nil,nil
-
-
-
 }
-
