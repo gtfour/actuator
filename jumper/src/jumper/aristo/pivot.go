119dda609fc219c895fa590151b568a07034a4b6
Marat Saldakov <wezeltonick@gmail.com>
2017-02-02 00:05:23 +0300

    hello

--- jumper/src/jumper/aristo/pivot.go
+++ jumper/src/jumper/aristo/pivot.go
@@ -1,42 +1,31 @@
 package aristo
 
 type Signa struct{
+    // atomic instance 
     Sid         string
     PrimaryGid  string
     Stype       int
 }
 
 type Membership struct {
-    //
-    Mid    string
-    Mtype  int
-    //
-    //
-    //
-    Gid    string
-    Gtype  int
-    //
+    Mid    string  // member id
+    Mtype  int     // member type
+    Gid    string  // group id
+    Gtype  int     // group type
 }
 
 type Cord struct {
-    //
-    //
-    // cord show's access rights(relation) between an requestor(Req) and resource(Tgt)
-    //
-    //
-    signaReqId          string
-    signaReqType        int
-    // signaReqGroupId  string
-    //
-    //
-    signaTgtId          string
-    signaTgtType        int
-    // signaTgtGroupId  string
-    //
-    //
-    //
+    //cord show's access rights(relation) between an requestor(Req) and resource(Tgt)
+    signaReqId         string
+    signaReqType       int
+    //signaReqGroupId  string
+    signaTgtId         string
+    signaTgtType       int
+    //signaTgtGroupId  string
+    cordType           int
 }
 
 func CheckAccess(signaReq *Signa, signaTgt *Signa)(state int){
     return SirAccessAllowed
 }
+//func GetDefaultStrategy
--- jumper/src/jumper/aristo/session.go
+++ jumper/src/jumper/aristo/session.go
@@ -5,11 +5,10 @@ import "jumper/cross"
 var SESSION_IS_VALID  int = 6000
 var SESSION_NOT_VALID int = 6001
 
-var session,session_open_error = OpenSession()
-
+var session, session_open_error = OpenSession()
 
 type Session struct {
-    cross.Database
+    db cross.Database
 }
 
 func OpenSession()(s *Session, err error){
