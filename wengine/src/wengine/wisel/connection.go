f47468ec39e53dd4c2b75e02708a5c50a68edd2b
Marat Saldakov <wezeltonick@gmail.com>
2017-06-02 11:08:38 +0300

    hello

--- wengine/src/wengine/wisel/connection.go
+++ wengine/src/wengine/wisel/connection.go
@@ -1,19 +1,34 @@
 package wisel
 
+type Session struct {
+    //
+    connections []*Connection
+    //
+}
+
 type Connection struct {
     //
-
-    userid        string   // websocket client id
-    subscriptions []string // this is a list with dashboard source id's ( dynima id's )
-    active        bool
-
+    id                  string   //
+    clientId            string   // websocket client id
+    subscriptionsALL    []string // this is a list with dashboard source id's (dynima id's)
+    subscriptionsVIEWED []string // dashboards that's open now on client side
+    active              bool     // connection is active
     //
 }
 
 type Dynima struct {
     //
+}
 
 
-
+func( s *Session )NewConnection( clientId string )(*Connection){
+    //
+    var c Connection
+    //
+    return &c
     //
 }
+
+
+
+//func( c *Connection )
